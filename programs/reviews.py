c49db9b6f13268b6281fc97c87e70c9d88497029
Medhavi Math <medhavimath744@gmail.com>
2023-10-13 02:35:43 +0530

    Delete programs/reviews.py

--- programs/reviews.py
+++
@@ -1,8 +0,0 @@
-from dbutils import result_as_dict
-
-
-def get_all_reviews(cursor):
-    cursor.execute('select * from reviews')
-    results=result_as_dict(cursor)
-    for res in results:
-            print(res)
